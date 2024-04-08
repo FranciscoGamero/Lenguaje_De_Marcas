10e4c8f1f84f3b0fe47d2432228765a97394a4a5
Francisco Manuel Gamero Rodríguez <gamero.rofra24@triana.salesianos.edu>
2024-04-08 12:30:12 +0200

    Ejercicio

---
+++ Apuntes y ejemplos/TEMA 9/Ejemplo2/codEjemplo.js
@@ -0,0 +1,14 @@
+let elDiv = document.querySelectorAll('div');
+elDiv.addEventListener('mouseOver',ratonSobre);
+
+function ratonSobre(elEvento){
+    //let elH1 = this.querySelector('h1');
+
+    let elH1 = this.firstElementChild;
+
+    //let elH1 = this.children[0];
+
+    //let elH1 = elEvento.currentTarget.firstElementChild;
+
+    elH1.hidden = false;
+}
